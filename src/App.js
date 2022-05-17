2b019cfd96135dcb84bab790e0a55d5daa6a02c6
kawther boukamcha <boukamchakawther@gmail.com>
2022-05-17 23:13:34 +0100

    formulaire de connexion

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="limiter">
+      <div
+        className="container-login100"
+        style={{ backgroundImage: 'url("images/bg-01.jpg")' }}
+      >
+        <div className="wrap-login100">
+          <form className="login100-form validate-form">
+            <span className="login100-form-logo">
+              <i className="zmdi zmdi-landscape" />
+            </span>
+            <span className="login100-form-title p-b-34 p-t-27">Log in</span>
+            <div
+              className="wrap-input100 validate-input"
+              data-validate="Enter username"
+            >
+              <input
+                className="input100"
+                type="text"
+                name="username"
+                placeholder="Username"
+              />
+              <span className="focus-input100" data-placeholder="" />
+            </div>
+            <div
+              className="wrap-input100 validate-input"
+              data-validate="Enter password"
+            >
+              <input
+                className="input100"
+                type="password"
+                name="pass"
+                placeholder="Password"
+              />
+              <span className="focus-input100" data-placeholder="" />
+            </div>
+            <div className="contact100-form-checkbox">
+              <input
+                className="input-checkbox100"
+                id="ckb1"
+                type="checkbox"
+                name="remember-me"
+              />
+              <label className="label-checkbox100" htmlFor="ckb1">
+                Remember me
+              </label>
+            </div>
+            <div className="container-login100-form-btn">
+              <button className="login100-form-btn">Login</button>
+            </div>
+            <div className="text-center p-t-90">
+              <a className="txt1" href="#">
+                Forgot Password?
+              </a>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
+    
+  </>
+  
+
   );
 }
 
